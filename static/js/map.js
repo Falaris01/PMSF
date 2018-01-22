2876e1cbbd11e76bc6652a1b850a37f2d7a4c4f5
Falaris <no@email.com>
2018-01-22 21:21:20 +0100

    Fixed double Park if team = 0

--- static/js/map.js
+++ static/js/map.js
@@ -633,9 +633,6 @@ function gymLabel(item) {
             '<div>' +
             lastScannedStr +
             '</div>' +
-            '<div>' +
-            park +
-            '</div>' +
             '</center>' +
             '</div>'
     } else {
