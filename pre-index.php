c9c3dc706a867b42a311ad64d145825f30f38302
Falaris <no@mail.de>
2018-02-06 15:41:38 +0100

    fix margin

--- pre-index.php
+++ pre-index.php
@@ -152,7 +152,7 @@ if ($blockIframe) {
         <div>
             <center>
                 <a href="https://pokemon-go-fans.de/" target="_blank" style="margin-top: 2px; vertical-align: middle;padding:0 5px;">
-                <img src="static/images/news.png" title="https://pokemon-go-fans.de/" border="0" style="display: inline-block; margin-top: -6px; max-width:100%; height:auto;">
+                <img src="static/images/news.png" title="https://pokemon-go-fans.de/" border="0" style="display: inline-block; margin-top: -4px; padding:-2px -2px 0 0; max-width:100%; height:auto;">
                 </a>
             </center>
         </div>
@@ -160,7 +160,7 @@ if ($blockIframe) {
             <center>
                 <?php
                 echo '<a href="' . $discordUrl . '" target="_blank" style="margin-top: 2px; vertical-align: middle;padding:0 5px;">
-                <img src="static/images/discord.png" border="0" title="Pokémon GO ' . $title . ' Discord" border="0" style="display: inline-block; margin-top: -6px; max-width:100%; height:auto;">
+                <img src="static/images/discord.png" border="0" title="Pokémon GO ' . $title . ' Discord" border="0" style="display: inline-block; margin-top: -4px; padding:-2px -2px 0 0; max-width:100%; height:auto;">
                 </a>'; ?>
             </center>
         </div>
@@ -168,7 +168,7 @@ if ($blockIframe) {
             <center>
                 <?php
                 echo '<a href="' . $nestMapUrl . '" target="_blank" style="margin-top: 2px; vertical-align: middle;padding:0 5px;">
-                <img src="static/images/nester.png" title="Nest Map by @shimpooky" border="0" style="display: inline-block; margin-top: -6px; max-width:100%; height:auto;">
+                <img src="static/images/nester.png" title="Nest Map by @shimpooky" border="0" style="display: inline-block; margin-top: -4px; padding:-2px -2px 0 0; max-width:100%; height:auto;">
                 </a>'; ?>
             </center>
         </div>
@@ -176,7 +176,7 @@ if ($blockIframe) {
             <center>
                 <?php
                 echo '<a href="' . $paypalUrl . '" target="_blank" style="margin-top: 2px; vertical-align: middle;padding:0 5px;">
-                <img src="static/images/spende.png" border="0" title="PayPal Spende" style="display: inline-block; margin-top: -6px; max-width:100%; height:auto;">
+                <img src="static/images/spende.png" border="0" title="PayPal Spende" style="display: inline-block; margin-top: -4px; padding:-2px -2px 0 0; max-width:100%; height:auto;">
                 </a>'; ?>
             </center>
         </div>
