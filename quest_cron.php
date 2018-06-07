0b39520d91a3369a62ba4e9f221ce7c2cb2fb781
Falaris <no@mail.de>
2018-06-07 22:34:46 +0200

    more fixes

--- quest_cron.php
+++ quest_cron.php
@@ -2,10 +2,5 @@
 include(dirname(__FILE__) . '/config/config.php');
 global $map, $fork, $db, $noManualQuests;
 
-if($noManualQuests === true){
-    http_response_code(401);
-    die();
-}
-
 $db->update('pokestops',['quest_id' => null, 'reward' => null, 'users' => null]);
 echo 'updated pokestops';
